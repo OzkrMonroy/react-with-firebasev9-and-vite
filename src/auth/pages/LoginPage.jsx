e72e5f211074e490465eac43e9da2cbd22c0d472
Oscar Monroy <oscarmonroy657@gmail.com>
2022-12-17 11:27:33 -0600

    Add AuthLayout and Register and Login forms

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,50 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <AuthLayout title="Login">
+      <form>
+        <Grid container sx={{ mb: 5 }}>
+          <Grid item xs={12} sx={{ mb: 3 }}>
+            <TextField
+              label="Email"
+              name="email"
+              placeholder="example@email.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb: 2 }}>
+          <Grid item xs={12} md={6}>
+            <Button variant="contained" fullWidth>
+              Login
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Button variant="contained" fullWidth>
+              <Google />
+              <Typography sx={{ ml: 2 }}>Google</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justifyContent="flex-end">
+          <Link component={RouterLink} color="inherit" to={"/auth/register"}>
+            Create an account
+          </Link>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,53 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container sx={{ mb: 5 }}>
+          <Grid item xs={12} sx={{ mb: 3 }}>
+            <TextField
+              label="Full name"
+              name="name"
+              placeholder="Pedro Pastor"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 3 }}>
+            <TextField
+              label="Email"
+              name="email"
+              placeholder="example@email.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb: 2 }}>
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth>
+              Create account
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justifyContent="flex-end">
+          <Typography sx={{ mr: 1 }}>Have already an account?</Typography>
+          <Link component={RouterLink} color="inherit" to={"/auth/login"}>
+            Login
+          </Link>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
