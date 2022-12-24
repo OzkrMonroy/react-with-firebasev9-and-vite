e79347b82e8e0554e6e8ce0faacecb136fecd412
Oscar Monroy <oscarmonroy657@gmail.com>
2022-12-23 20:42:28 -0600

    Add validators to the useForm
    Fields of Register page has been setup with our custom hook to handle forms

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,34 +1,84 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
-import { Google } from "@mui/icons-material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+
+const formValidations = {
+  email: [(value) => value.includes("@"), "El email debe de contener una @"],
+  password: [
+    (value) => value.length >= 6,
+    "La contraseña debe de poseer más de 6 caracteres",
+  ],
+  name: [(value) => value.length > 1, "El nombre es obligatorio"],
+};
 
 export const RegisterPage = () => {
+  const {
+    name,
+    email,
+    password,
+    nameValid,
+    emailValid,
+    passwordValid,
+    formChanged,
+    onInputChange,
+  } = useForm(
+    {
+      name: "Test vite",
+      email: "test20@test.com",
+      password: "1234567",
+    },
+    formValidations
+  );
+
+  console.log({
+    nameValid,
+    emailValid,
+    passwordValid,
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setformChanged(true);
+  };
+
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container sx={{ mb: 5 }}>
           <Grid item xs={12} sx={{ mb: 3 }}>
             <TextField
               label="Full name"
-              name="name"
               placeholder="Pedro Pastor"
+              name="name"
+              value={name}
+              onChange={onInputChange}
+              error={!!nameValid && formChanged}
+              helperText={nameValid}
               fullWidth
             />
           </Grid>
           <Grid item xs={12} sx={{ mb: 3 }}>
             <TextField
               label="Email"
-              name="email"
               placeholder="example@email.com"
+              name="email"
+              value={email}
+              error={!!emailValid && formChanged}
+              helperText={emailValid}
+              onChange={onInputChange}
               fullWidth
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
               label="Password"
-              name="password"
               type="password"
+              name="password"
+              value={password}
+              error={!!passwordValid && formChanged}
+              helperText={passwordValid}
+              onChange={onInputChange}
               fullWidth
             />
           </Grid>
@@ -36,7 +86,7 @@ export const RegisterPage = () => {
 
         <Grid container spacing={2} sx={{ mb: 2 }}>
           <Grid item xs={12}>
-            <Button variant="contained" fullWidth>
+            <Button type="submit" variant="contained" fullWidth>
               Create account
             </Button>
           </Grid>
